881920cf34d82b698f58d627632cb44c770caa58
Sergio de Miguel <99650746+camaron-de-la-isla@users.noreply.github.com>
2025-12-17 13:55:57 +0100

    loginscreen y registerscreen
    falta la la validacion de credenciales

---
+++ Frontend/js/errorRegister.js
@@ -0,0 +1,24 @@
+const nombre = document.getElementById('nombre');
+const apellidos = document.getElementById('apellidos');
+const user = document.getElementById('user');
+const email = document.getElementById('email');
+const pass = document.getElementById('password');
+const form = document.getElementById('register-form');
+const error = document.getElementById('register-error');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    if(!nombre.value || !apellidos.value || !user.value || !email.value || !pass.value){
+        mostrarError('Rellene todos los campos');
+        return;
+    }
+
+    error.style.display = 'none';
+    form.submit();
+});
+
+function mostrarError(message){
+    error.textContent = message;
+    error.style.display = 'block';
+}
---
+++ Frontend/js/passVisibility.js
@@ -0,0 +1,10 @@
+const toggle = document.getElementById("togglePassword");
+const password = document.getElementById("password");
+
+toggle.addEventListener("click", () => {
+    const visible = password.type === "text";
+    password.type = visible ? "password" : "text";
+    toggle.src = visible
+    ? "img/icons/visibility_off.svg"
+    : "img/icons/visibility_on.svg";
+});
